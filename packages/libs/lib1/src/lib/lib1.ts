daa4c9e9acb5196b3a499784bfea0cd8679ab7e8
Nisan <nnisan858@gmail.com>
2025-04-25 09:59:17 +0300

    examples libs

---
+++ packages/libs/lib1/src/lib/lib1.ts
@@ -0,0 +1,6 @@
+import { lib2 } from '@zeta/lib2';
+
+export function lib1(): string {
+    console.log(lib2());
+    return 'lib1';
+}
---
+++ packages/libs/lib2/src/lib/lib2.spec.ts
@@ -0,0 +1,7 @@
+import { lib2 } from './lib2.js';
+
+describe('lib2', () => {
+    it('should work', () => {
+        expect(lib2()).toEqual('lib2');
+    });
+});
